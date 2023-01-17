39892242083d39b593eb02b9d11a036d600168e9
abhimanyu <116670401+abhimanyu-1@users.noreply.github.com>
2023-01-17 18:59:42 +0530

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -1,60 +1,80 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
+#define INFINITY 9999
+#define MAX 10
+ 
+void dijkstra(int G[MAX][MAX],int n,int startnode);
+ 
 int main()
 {
-  int i,j,n,s,cost[10][10],dist[10];
-  printf("Enter  the number of nodes: ");
-  scanf("%d",&n);
-  printf("Enter cost matrix: \n");
-  for(i=1 ; i<=n ; i++)
-  {
-   for(j=1 ; j<=n ; j++)
-   {
-    scanf("%d",&cost[i][j]);
-    if(cost[i][j] == 0)
-      cost[i][j] = 999 ;
-   }
-  }
-  printf("source vertex:");
-  scanf("%d",&s);
-  dijkstras(n,cost,s,dist);
-  printf("shortest path from %d is:\n",s);
-  for(i=1 ; i<=n ; i++)
-  {
-   if(s!=i)
-   {
-    printf("%d -> %d = %d \n" , s,i,dist[i]);
-   }
-  }
- return 0;
+int G[MAX][MAX],i,j,n,u;
+printf("Enter no. of vertices:");
+scanf("%d",&n);
+printf("\nEnter the adjacency matrix:\n");
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+scanf("%d",&G[i][j]);
+printf("\nEnter the starting node:");
+scanf("%d",&u);
+dijkstra(G,n,u);
+return 0;
+}
+ 
+void dijkstra(int G[MAX][MAX],int n,int startnode)
+{
+ 
+int cost[MAX][MAX],distance[MAX],pred[MAX];
+int visited[MAX],count,mindistance,nextnode,i,j;
 
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+if(G[i][j]==0)
+cost[i][j]=INFINITY;
+else
+cost[i][j]=G[i][j];
+
+for(i=0;i<n;i++)
+{
+distance[i]=cost[startnode][i];
+pred[i]=startnode;
+visited[i]=0;
+}
+distance[startnode]=0;
+visited[startnode]=1;
+count=1;
+while(count<n-1)
+{
+mindistance=INFINITY;
+
+for(i=0;i<n;i++)
+if(distance[i]<mindistance&&!visited[i])
+{
+mindistance=distance[i];
+nextnode=i;
 }
 
-
-void dijkstras(int n,int cost[10][10],int s,int dist[10])
+visited[nextnode]=1;
+for(i=0;i<n;i++)
+if(!visited[i])
+if(mindistance+cost[nextnode][i]<distance[i])
 {
- int i,v,count= 1,min ,visited[10];
- for(i=1 ; i<=n; i++)
- {
-  visited[i] = 0;
-  dist[i] = cost[s][i];
- }
- visited[s] = 1;
- dist[s] = 0;
- while(count <= n)
- {
-  min = 999;
-  for(i=1 ; i<=n ; i++)
-   if(dist[i] < min && visited[i] == 0)
-   {
-    min = dist[i];
-    v = i;
-   }
-   visited[v] = 1;
-   count++;
-   for(i=1 ; i<=n ; i++)
-    if(dist[i] > dist[v] + cost[v][i])
-     dist[i] = dist[v] + cost[v][i];
-  }
- }
+distance[i]=mindistance+cost[nextnode][i];
+pred[i]=nextnode;
+}
+count++;
+}
  
+
+for(i=0;i<n;i++)
+if(i!=startnode)
+{
+printf("\nDistance of node%d=%d",i,distance[i]);
+printf("\nPath=%d",i);
+j=i;
+do
+{
+j=pred[j];
+printf("<-%d",j);
+}while(j!=startnode);
+}
+}
